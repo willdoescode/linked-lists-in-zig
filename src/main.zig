7a5184d076bc39b9cc41aabd5721eccd8f05d612
Will Lane <williamlane923@gmail.com>
2021-04-03 18:23:54 +0200

    append

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 const singly = @import("./singly.zig");
+
 const assert = std.debug.assert;
 const expectSlice = std.testing.expectEqualSlices;
 
-pub fn main() !void {
-}
+pub fn main() !void {}
 
 test "init singly" {
     var num = singly.Node(i32).init(5);
@@ -12,4 +12,15 @@ test "init singly" {
 
     var str = singly.Node([]const u8).init("Hello");
     expectSlice(u8, "Hello", str.val);
+}
+
+test "append" {
+    var depth1 = singly.Node(?u8).init(null);
+    depth1.append(6);
+    assert(depth1.next.?.val.? == 6);
+
+    var depth2 = singly.Node(?u8).init(null);
+    depth2.append(null);
+    depth2.append(5);
+    assert(depth2.next.?.next.?.val.? == 5);
 }
--- src/singly.zig
+++ src/singly.zig
@@ -2,26 +2,31 @@ const std = @import("std");
 const print = std.debug.print;
 
 pub fn Node(comptime T: type) type {
-  return struct {
-    const Self = @This();
+    return struct {
+        const Self = @This();
 
-    val: T,
-    next: ?*Self,
+        val: T,
+        next: ?*Self,
 
-    pub fn init(val: T) Self {
-      return Self {
-        .val = val,
-        .next = null
-      };
-    }
-  };
+        pub fn init(val: T) Self {
+            return Self{ .val = val, .next = null };
+        }
+
+        pub fn append(self: *Self, val: T) void {
+            if (self.*.next) |node| {
+                node.append(val);
+            } else {
+                self.*.next = &Node(T).init(val);
+            }
+        }
+    };
 }
 
-  // pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
-    // var hold = self;
-    // try writer.print("{d} ->", .{self.val});
-    // while (hold.next) |n| {
-    //   hold = n.*;
-    //   try writer.print("{d}->", .{hold.val});
-    // }
-  // }
+// pub fn format(self: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+// var hold = self;
+// try writer.print("{d} ->", .{self.val});
+// while (hold.next) |n| {
+//   hold = n.*;
+//   try writer.print("{d}->", .{hold.val});
+// }
+// }
