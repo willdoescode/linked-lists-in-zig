79f9585455e9d95e681445535b7bff6e906f99be
Will Lane <williamlane923@gmail.com>
2021-05-16 03:01:42 -0700

    heap allocations

--- src/main.zig
+++ src/main.zig
@@ -1,35 +1,53 @@
 const std = @import("std");
-const singly = @import("./singly.zig");
+const print = std.debug.print;
 
-const assert = std.testing.expect;
-const test_allocator = std.testing.allocator;
-const expectSlice = std.testing.expectEqualSlices;
+const Node = struct {
+    const Self = @This();
+
+    val: i32,
+    next: ?*Node,
+    allocator: *std.mem.Allocator,
+
+    fn init(val: i32, allocator: *std.mem.Allocator) Self {
+        return Self{ .val = val, .next = null, .allocator = allocator };
+    }
+
+    fn append(self: *Self, val: i32) anyerror!void {
+        if (self.next) |next_node| {
+            try next_node.append(val);
+        } else {
+            var node = try self.allocator.create(Node);
+            node.* = Node.init(val, self.allocator);
+
+            self.next = node;
+        }
+    }
+
+    pub fn format(self: *const Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
+        var cur = self;
+        try writer.print("{} ", .{self.val});
+        while (cur.next) |next| : (cur = next) {
+            try writer.print("-> {} ", .{next.val});
+        }
+    }
+
+    fn deinit(self: *Self) void {
+        self.allocator.destroy(self);
+    }
+};
 
 pub fn main() !void {
-    var list = singly.Node([]const u8).init("Hello");
-    list.append("World!");
-    std.debug.print("{s}\n", .{list});
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = &arena.allocator;
+
+    var list = Node.init(5, allocator);
+    defer list.deinit();
+
+    try list.append(6);
+    try list.append(7);
+
+    print("{}\n", .{list});
 }
 
-
-test "init singly" {
-    var num = singly.Node(i32).init(5);
-    assert(num.val == 5);
-
-    var str = singly.Node([]const u8).init("Hello");
-    expectSlice(u8, "Hello", str.val);
-}
-
-test "append" {
-    var depth1 = singly.Node(?u8).init(null);
-    depth1.append(6);
-    assert(depth1.val == null);
-    assert(depth1.next.?.val.? == 6);
-
-    var depth2 = singly.Node(u8).init(1);
-    depth2.append(2);
-    depth2.append(3);
-    assert(depth2.val == 1);
-    assert(depth2.next.?.val == 2);
-    assert(depth2.next.?.next.?.val == 3);
-}
