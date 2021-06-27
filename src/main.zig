3867fc4271d9b5a3160fe41c90c3180c13f0133a
Will Lane <williamlane923@gmail.com>
2021-06-27 16:23:13 -0500

    list to manage memory

--- src/main.zig
+++ src/main.zig
@@ -6,56 +6,64 @@ const Node = struct {
 
     val: i32,
     next: ?*Node,
-    allocator: *std.mem.Allocator,
 
-    fn init(val: i32, allocator: *std.mem.Allocator) Self {
-        return Self{ .val = val, .next = null, .allocator = allocator };
+    fn init(val: i32) Self {
+        return Self { .val = val, .next = null };
     }
 
-    fn fromList(list: []const i32, allocator: *std.mem.Allocator) anyerror!Self {
-        var ret = Self{ .val = list[0], .allocator = allocator, .next = null };
-
-        for (list[1..]) |item| {
-            try ret.append(item);
-        }
-
-        return ret;
+    fn deinit(self: *Self, previous: ?*Self, allocator: *std.mem.Allocator) void {
+        if (previous) |p| allocator.destroy(p);
+        if (self.next) |next| next.deinit(self, allocator)
+        else allocator.destroy(self);
     }
 
-    fn append(self: *Self, val: i32) anyerror!void {
-        if (self.next) |next_node| {
-            try next_node.append(val);
-        } else {
-            var node = try self.allocator.create(Node);
-            node.* = Node.init(val, self.allocator);
-
+    fn append(self: *Self, val: i32, allocator: *std.mem.Allocator) anyerror!void {
+        if (self.next) |next| try next.append(val, allocator)
+        else {
+            var node = try allocator.create(Node);
+            node.* = Node.init(val);
             self.next = node;
         }
     }
+};
+
+// Manage allocator
+const List = struct {
+    const Self = @This();
+
+    head: *Node,
+    allocator: *std.mem.Allocator,
+
+    fn init(val: i32, allocator: *std.mem.Allocator) Self {
+        var node = allocator.create(Node) catch |_| @panic("Could not create Node");
+        node.* = Node.init(val);
+        return Self { .head = node, .allocator = allocator };
+    }
+
+    fn deinit(self: *Self) void { self.head.deinit(null, self.allocator); }
+
+    fn append(self: *Self, val: i32) void { 
+        self.head.append(val, self.allocator) 
+        catch |_| @panic("Could not append"); 
+    }
 
     pub fn format(self: *const Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
-        var cur = self;
-        try writer.print("{}", .{self.val});
+        var cur = self.head;
+        try writer.print("{}", .{cur.val});
         while (cur.next) |next| : (cur = next) {
             try writer.print(" -> {}", .{next.val});
         }
     }
-
-    fn deinit(self: *Self) void {
-        self.allocator.destroy(self);
-    }
 };
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(!gpa.deinit());
+    const allocator = &gpa.allocator;
 
-    const allocator = &arena.allocator;
-
-    var list = try Node.fromList(&[_]i32{ 1, 2, 3, 4 }, allocator);
+    var list = List.init(1, allocator);
     defer list.deinit();
-
-    try list.append(5);
+    {var i: usize = 2; while (i <= 10) : (i += 1) { list.append(@intCast(i32, i)); }}
 
     print("{}\n", .{list});
 }
